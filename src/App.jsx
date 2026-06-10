09a614f80c54a654f57c6c3224adde357c925dd9
Irati98o <irati98o@hotmail.es>
2026-06-10 12:54:58 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+export default function App() {
+
+  const [user, setUser] = useState(null);
+
+  if (!user) return <Login setUser={setUser} />;
+
+  return <Home user={user} />;
+}
