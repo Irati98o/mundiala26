6b2b7608c468bfad62318c9bf79873c26af89ec0
Irati98o <irati98o@hotmail.es>
2026-06-10 13:34:58 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect, useState } from "react";
 import { db } from "../firebase/config";
 import { collection, onSnapshot, addDoc } from "firebase/firestore";
