6980e8a78ae7cd6a819a4d9607d11f77d4085878
Irati98o <irati98o@hotmail.es>
2026-06-10 13:58:50 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,23 +7,30 @@ export default function Login({ setUser }) {
   const [name, setName] = useState("");
 
   const login = async () => {
-    const q = query(collection(db, "users"), where("name", "==", name));
-    const existing = await getDocs(q);
+  const q = query(collection(db, "users"), where("name", "==", name));
+  const existing = await getDocs(q);
 
-    if (!existing.empty) {
-      alert("Nombre ya en uso");
-      return;
-    }
-
-    const doc = await addDoc(collection(db, "users"), {
-      name,
-      points: 0,
-      isAdmin: name === "Irati"
-    });
+  if (!existing.empty) {
+    // ✅ YA EXISTE → entrar
+    const doc = existing.docs[0];
 
     localStorage.setItem("userId", doc.id);
     setUser({ id: doc.id, name });
-  };
+
+    return;
+  }
+
+  // ✅ NO EXISTE → crear usuario nuevo
+  const docRef = await addDoc(collection(db, "users"), {
+    name,
+    points: 0,
+    koPoints: 0,
+    isAdmin: name === "Irati"
+  });
+
+  localStorage.setItem("userId", docRef.id);
+  setUser({ id: docRef.id, name });
+};
 
   return (
     <div className="center">
