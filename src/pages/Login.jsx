1f4b7262dbefacd2d1b2a5f3af3efd58ab05e12e
Irati98o <irati98o@hotmail.es>
2026-06-10 12:50:06 +0200

    Create Login.jsx

---
+++ src/pages/Login.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { db } from "../firebase/config";
+import { collection, addDoc, query, where, getDocs } from "firebase/firestore";
+
+export default function Login({ setUser }) {
+  const [name, setName] = useState("");
+
+  const login = async () => {
+    const q = query(collection(db, "users"), where("name", "==", name));
+    const existing = await getDocs(q);
+
+    if (!existing.empty) {
+      alert("Nombre ya en uso");
+      return;
+    }
+
+    const doc = await addDoc(collection(db, "users"), {
+      name,
+      points: 0,
+      isAdmin: name === "Irati"
+    });
+
+    localStorage.setItem("userId", doc.id);
+    setUser({ id: doc.id, name });
+  };
+
+  return (
+    <div className="center">
+      <input value={name} onChange={e => setName(e.target.value)} />
+      <button onClick={login}>Entrar</button>
+    </div>
+  );
+}
