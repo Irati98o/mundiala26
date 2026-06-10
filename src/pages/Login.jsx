9d93e3735c6c25e0d36e401d0dc673c1246c3a8a
Irati98o <irati98o@hotmail.es>
2026-06-10 13:35:39 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 import { db } from "../firebase/config";
 import { collection, addDoc, query, where, getDocs } from "firebase/firestore";
