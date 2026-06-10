01af26e965751bbe20329cce8068f0578fcc4077
Irati98o <irati98o@hotmail.es>
2026-06-10 14:47:29 +0200

    Update MatchCard.jsx

--- src/components/MatchCard.jsx
+++ src/components/MatchCard.jsx
@@ -22,9 +22,7 @@ export default function MatchCard({ match, onPredict, getUserPrediction }) {
       ) : userPrediction ? (
         <p>✅ Apostado: {userPrediction.prediction}</p>
         <div>
-          <button className={userPrediction?.prediction === "1" ? "active" : ""} onClick={() => onPredict(match.id, "1")}>1</button>
-          <button className={userPrediction?.prediction === "X" ? "active" : ""} onClick={() => onPredict(match.id, "X")}>X</button>
-          <button className={userPrediction?.prediction === "2" ? "active" : ""} onClick={() => onPredict(match.id, "2")}>2</button>
+          <h1>hola</h1>
         </div>
       ) : (
         <div>
